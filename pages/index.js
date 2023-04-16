49b81cbba59941c07dc836c4182c6c68bb0714b8
Kasodariya Deep <deepkasodariya44@gmail.com>
2023-04-16 12:16:31 +0530

    Added Minor Changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Navbar from '@/components/Navbar'
 import Welcome from '@/components/Welcome'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className='gradient-bg-welcome'>
+      <Head>
+        <title>Krypto Exchange</title>
+      </Head>
       <Navbar />
       <Welcome />
     </div>
